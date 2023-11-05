12e16ee9e635f978631721fa357d7099fbde6337
Huw Carpenter <huwcarpenter@gmail.com>
2023-11-05 09:17:04 +0000

    Add LibGDX-generated project files

---
+++ desktop/src/com/huwdunnit/spaceattack/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.huwdunnit.spaceattack;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.huwdunnit.spaceattack.SpaceAttackGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("space-attack");
+		new Lwjgl3Application(new SpaceAttackGame(), config);
+	}
+}
